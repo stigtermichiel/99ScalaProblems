758b18f1eb99d7e74ca4ed96f2a6a8a0608dd7b3
Michiel Stigter <michielstigter@Michiels-MacBook-Pro.local>
2014-10-13 14:46:29 +0100

    first 10 q's

--- 99-problems/src/main/scala/com/example/scalaproject/NinetyNineProblems.scala
+++ 99-problems/src/main/scala/com/example/scalaproject/NinetyNineProblems.scala
@@ -10,4 +10,63 @@ object NinetyNineProblems {
     case head :: tail => last(tail)
   }
 
+  def penultimate[A](ls: List[A]): A = ls match {
+    case Nil => throw new NoSuchElementException()
+    case head :: tail if tail.length == 1 => head
+    case head :: tail => penultimate(tail)
+  }
+
+  def nth[A](n: Int, ls: List[A]): A = (n, ls) match {
+    case (_, Nil) => throw new NoSuchElementException()
+    case (0, head :: tail) => head
+    case (_, head :: tail) => nth(n-1, tail)
+  }
+
+  def length[A](ls: List[A]): Int = {
+    def lengthRec(rs: List[A], counter: Int): Int = rs match {
+      case Nil => counter
+      case head :: tail => lengthRec(tail, counter + 1)
+      }
+    lengthRec(ls, 0)
+  }
+
+  def reverse[A](ls: List[A]): List[A] = {
+    def reverseRec(rs: List[A], result: List[A]): List[A] = rs match {
+      case Nil => result
+      case head :: tail => reverseRec(tail, result.+:(head))
+    }
+    reverseRec(ls, List())
+  }
+
+  def isPalindrome[A](ls: List[A]): Boolean = {
+    ls == ls.reverse
+  }
+
+  def flatten(ls: List[Any]): List[Any] = ls flatMap {
+    case ms: List[_] => flatten(ms)
+    case e => List(e)
+  }
+
+  def compress[A](ls: List[A]): List[A] = {
+    def compressRec(ms: List[A], result: List[A]): List[A] = (ms, result) match {
+      case (Nil, result) => result.reverse
+      case (head :: tail, h :: t) if head == h => compressRec(tail, h :: t)
+      case (head :: tail, result) => compressRec(tail, head :: result )
+    }
+    compressRec(ls, List())
+  }
+
+  def pack[Symbol](ls: List[Symbol]): List[List[Symbol]] = {
+    if (ls.isEmpty) List(List())
+    else {
+      val (done, todo) = ls span { _ == ls.head }
+      if (todo == Nil) List(done)
+      else done :: pack(todo)
+    }
+  }
+
+  def encode[Symbol](ls: List[Symbol]): List[(Int, Symbol)] = {
+   pack(ls) map {x => (x.length, x.head)}
+  }
+
 }
--- 99-problems/src/test/scala/com/example/scalaproject/unit/ProgramTest.scala
+++ 99-problems/src/test/scala/com/example/scalaproject/unit/ProgramTest.scala
@@ -6,9 +6,48 @@ import NinetyNineProblems._
 
 class ProgramTest extends FunSuite {
 
-  test("weight of a larger tree") {
+  test("Last element of List") {
       assert(last(List(1,2,3,4,5)) === 5)
     }
+
+  test("Last but one element") {
+    assert(penultimate(List(1, 1, 2, 3, 5, 8)) === 5)
+  }
+
+  test("Find the Kth element of a list") {
+    assert(nth(4, List(1, 1, 2, 3, 5, 8)) === 5)
+  }
+
+  test("Find the number of elements in a list") {
+    assert(length(List(1, 1, 2, 3, 5, 8, 2, 3, 4)) === 9)
+  }
+
+  test("Reverse a List") {
+    assert(reverse(List(1,1,2,3,5,8)) === List(8, 5, 3, 2, 1, 1) )
+  }
+
+  test("Find out whether a list is a palindrome.") {
+    assert(isPalindrome(List(1, 2, 3, 2, 1)) === true)
+  }
+
+  test("Flatten a nested list structure.") {
+    assert(flatten(List(List(1, 1), 2, List(3, List(5, 8)))) === List(1, 1, 2, 3, 5, 8) )
+  }
+
+  test("Eliminate consecutive duplicates of list elements.") {
+    assert(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+
+  test("Pack consecutive duplicates of list elements into sublists.") {
+    assert(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)) )
+  }
+
+  test("Run-length encoding of a list.") {
+    assert(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) === List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)) )
+  }
+
+
+
 
 
 
